742be1d17a0fa658814cf7e784b4c08254565e78
Vinay Parida <vinay.parida@greytip.com>
2025-01-03 01:45:45 +0530

    init repo

---
+++ src/main/java/com/vdp/localDateString/LocalDateStringApplication.java
@@ -0,0 +1,13 @@
+package com.vdp.localDateString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDateStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDateStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdp/localDateString/LocalDateStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdp.localDateString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDateStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
